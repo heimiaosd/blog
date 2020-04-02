46a0b34c31afcb9fbb520c32c9db3e0104d2d7f7
heimiaosd <2381709027@qq.com>
2020-04-02 18:49:24 +0800

    from commit

--- blog-后台/src/main/java/com/example/blog/interceptor/WebConfig.java
+++ blog-后台/src/main/java/com/example/blog/interceptor/WebConfig.java
@@ -1,9 +1,13 @@
 package com.example.blog.interceptor;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig extends WebMvcConfigurationSupport {
@@ -12,12 +16,18 @@ public class WebConfig extends WebMvcConfigurationSupport {
         registry.addInterceptor(new LoginInterceptor())
                 .addPathPatterns("/admin/**")
                 .excludePathPatterns("/admin")
-                .excludePathPatterns("/admin/login");
+                .excludePathPatterns("/admin/login")
+                .excludePathPatterns("/admin/blog");
     }
 
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations("/static/");
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    protected void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new PageableHandlerMethodArgumentResolver());
     }
 }
--- blog-后台/src/main/java/com/example/blog/web/admin/AdminBlogController.java
+++ blog-后台/src/main/java/com/example/blog/web/admin/AdminBlogController.java
@@ -3,12 +3,13 @@ package com.example.blog.web.admin;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
-@RequestMapping("/admin")
 public class AdminBlogController {
-    @GetMapping("/blog")
+    @GetMapping("/admin/blog")
     public String adminBlog(){
-        return "/admin/blog";
+        return "admin/blog";
     }
 }
